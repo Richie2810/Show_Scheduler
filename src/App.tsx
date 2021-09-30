2edabe222361a079ef1375efc66c587f68080065
Richie2810 <richiegoneill@gmail.com>
2021-09-30 12:43:39 +0200

    able to add performances to Fan's schedule

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,89 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Button, Col, Container, Form } from "react-bootstrap";
 import "./App.css";
-import Login from "./components/Login";
 import Performances from "./components/Performances";
 // import { PerformancesProps } from "./components/Performances";
 import { apiUrl } from "./config/constants";
 
 function App() {
+  const [name, set_name] = useState("");
   const [allPerf, setAllPerf] = useState([]);
   useEffect(() => {
     async function getPerformances() {
       const response = await axios.get(`${apiUrl}/allPerformances`);
+      console.log(response.data);
       setAllPerf(response.data);
     }
     getPerformances();
   }, []);
 
-  // async function getPerformances() {
-  //   const response = await axios.get(`${apiUrl}/allPerformances`);
-  //   return response.data;
-  // }
+  async function submitName() {
+    console.log(name);
+
+    const response = await axios.post(
+      `${apiUrl}/newFan`,
+      {
+        name,
+      },
+      {}
+    );
+    console.log(response.data);
+  }
+
+  const onSchduleClick = async (id) => {
+    console.log(id);
+    const response = await axios.patch(
+      `${apiUrl}/addToSchedule`,
+      {
+        name,
+        performance: id,
+      },
+      {}
+    );
+    console.log(response.data);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+        <Container>
+          <Form
+            as={Col}
+            md={{ span: 6, offset: 3 }}
+            className="mt-5"
+            style={{ backgroundColor: "#557A95" }}
+          >
+            <h1 className="mt-5 mb-5">Login</h1>
+            <Form.Group controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                value={name}
+                onChange={(event) => set_name(event.target.value)}
+                type="name"
+                placeholder="Enter Name"
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mt-5">
+              <Button variant="primary" type="submit" onClick={submitName}>
+                Log in
+              </Button>
+            </Form.Group>
+          </Form>
+        </Container>
         {allPerf
-          ? allPerf.map((perf, key) => {
+          ? allPerf.map((perf) => {
               return (
                 <Performances
-                  key={key}
+                  key={perf._id}
+                  id={perf._id}
                   title={perf.title}
                   start_date={perf.start_date}
                   end_date={perf.end_date}
                   description={perf.description}
                   status={perf.status}
+                  onSchduleClick={onSchduleClick}
                 />
               );
             })
--- src/components/Performances.tsx
+++ src/components/Performances.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Dropdown, DropdownButton } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 
 export interface PerformancesProps {
   title: String;
@@ -7,10 +7,15 @@ export interface PerformancesProps {
   description: String;
   status: String;
   key: Number;
+  id: String;
+  onSchduleClick: Function;
 }
 
 export default function Performances(props: PerformancesProps) {
-  console.log(props);
+  const onScheduleClick = (id) => {
+    props.onSchduleClick(id);
+  };
+
   return (
     <Col>
       <Card className="mb-3">
@@ -18,9 +23,14 @@ export default function Performances(props: PerformancesProps) {
         <Card.Text>{props.description}</Card.Text>
         <Card.Text>Start: {props.start_date}</Card.Text>
         <Card.Text>End: {props.end_date}</Card.Text>
-        <DropdownButton id="dropdown-item-button" title="Add to Schedule">
-          <Dropdown.Item as="button" />
-        </DropdownButton>
+        <button
+          type="button"
+          onClick={() => {
+            onScheduleClick(props.id);
+          }}
+        >
+          Add to schedule
+        </button>
       </Card>
     </Col>
   );
